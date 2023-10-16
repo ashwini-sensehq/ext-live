647e145c7987c8cd3261d50c21162820794ecc2c
Ashwini Kemshetty <21ashwinirao@gmail.com>
2023-10-16 23:00:10 +0530

    correct content script

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -4,7 +4,7 @@ window.onload = async () => {
     const header = document.querySelector("h1");
   
     console.log("Inside the content file");
-    if (h1) {
+    if (header) {
       console.log(header);
       header.innerText = "Hello, this is from sense on page load";
     }
