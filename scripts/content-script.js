a522dd83ae9e8092e488ad99ba835653e1250288
Ashwini Kemshetty <21ashwinirao@gmail.com>
2023-10-16 22:59:28 +0530

    content script with error

---
+++ scripts/content-script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+window.onload = async () => {
+    const header = document.querySelector("h1");
+  
+    console.log("Inside the content file");
+    if (h1) {
+      console.log(header);
+      header.innerText = "Hello, this is from sense on page load";
+    }
+}
