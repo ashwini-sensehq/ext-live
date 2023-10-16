f9aeb5459f69f10abea0caba797ecb72045a0d05
Ashwini Kemshetty <21ashwinirao@gmail.com>
2023-10-16 23:39:34 +0530

    show notification badge

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
 chrome.runtime.onMessage.addListener((message) => {
     if (message.type === "UPDATE_BADGE") {
         // change to tagname 
-        chrome.action.setBadgeText({ text: message.text.length });
+        chrome.action.setBadgeBackgroundColor({color: '#ff0000'});
+        chrome.action.setBadgeText({ text: message.text.length.toString() });
     }
 });
