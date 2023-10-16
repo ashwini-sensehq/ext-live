2b037e03e3c7152f2d3d9177414e203a103c3b96
Ashwini Kemshetty <21ashwinirao@gmail.com>
2023-10-16 23:07:32 +0530

    popup setup

---
+++ popup/popup.js
@@ -0,0 +1,14 @@
+"use strict";
+
+window.onload = async () => {
+  // update title
+  const updateTitleButton = document.getElementById("updateTitle");
+  const titleText = document.getElementById("titleText");
+  updateTitleButton.addEventListener("click", () => {
+
+    // logic to update the title
+
+
+  });
+
+};
